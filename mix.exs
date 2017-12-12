97ee16e399a18b5b58fbc0e32ee79fa69a9f64fe
Domingo Aguilera <domingo@britecore.com>
2017-12-11 18:25:13 -0600

    ember build and change to cowboy 2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExqUi.Mixfile do
   def project do
     [ app: :exq_ui,
       version: "0.9.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.5",
       elixirc_paths: ["lib", "web"],
       package: [
         maintainers: ["Alex Kira", "Justin McNally", "Nick Sanders"],
@@ -34,11 +34,11 @@ defmodule ExqUi.Mixfile do
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [
-      { :exq, "~> 0.9"},
-      { :plug, ">= 0.8.1 and < 2.0.0"},
-      { :cowboy, "~> 1.0" },
+      { :exq, "~> 0.9" },
+      { :plug, github: "elixir-lang/plug" },
+      { :cowboy, "~> 2.0" },
       { :excoveralls, "~> 0.3", only: :test },
-      {:ex_doc, ">= 0.0.0", only: :dev}
+      { :ex_doc, ">= 0.0.0", only: :dev }
     ]
   end
 end
